1ae5a4180aba13d583f42f34ee23679262498422
Tobias Valderhaug <tobiasvalderhaug@Tobiass-MacBook-Pro.local>
2020-12-10 14:53:18 +0100

    Version 1.4

--- Oppgave 2 kode/RX/sketch_dec10b/sketch_dec10b.ino
+++ Oppgave 2 kode/RX/sketch_dec10b/sketch_dec10b.ino
@@ -1,23 +1,20 @@
 void setup() {
   Serial.begin(9600);
   pinMode(8, OUTPUT);
- }
- int incomingByte = 0;
- // for incoming serial data
- void loop() {
+}
+int incomingByte = 0; // for incoming serial data
+void loop() {
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
     // say what you got:
     Serial.print("I received: ");
     Serial.println(incomingByte, DEC);
-    }
-    if (incomingByte == 1) {
-      digitalWrite(8, HIGH);
-      }
-      else {
-        digitalWrite(8, LOW);
-        }
-      }
   }
- }
+  if (incomingByte == 1) {
+    digitalWrite(8, HIGH);
+  }
+  else {
+    digitalWrite(8, LOW);
+  }
+}
